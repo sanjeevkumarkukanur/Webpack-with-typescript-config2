a68b2dfd993bb0b10b380c23dd0ab4fde3adfd21
sanjeevkumarkukanur <sanjeev.kukanur@gmail.com>
2024-01-27 17:44:47 +0530

    webpack

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+const name= "sanjee"
+  return (
+    <div>
+      webpacK APP {process.env.name}
+      <img src='https://media.istockphoto.com/id/1382384282/photo/bangalore-or-bengaluru.jpg?s=612x612&w=0&k=20&c=6pxwL3JxNV2B_NZSLMZLhrSLqAbyCPlGuSZYKImpjKQ=' alt='' width={400} height={400} ></img>
+    </div>
+  )
+}
+
+export default App
